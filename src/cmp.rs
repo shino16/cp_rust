a499df35ca037647c59e82f4afc12096395b7072
shino16 <masatosinokawa@gmail.com>
2020-11-03 08:01:44 +0900

    .

--- src/cmp.rs
+++ src/cmp.rs
@@ -1,10 +1,1 @@
-#[derive(PartialEq, PartialOrd)]
-pub struct Total<T>(pub T);
-
-impl<T: PartialEq> Eq for Total<T> {}
-
-impl<T: PartialOrd> Ord for Total<T> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.0.partial_cmp(&other.0).unwrap()
-    }
-}
+pub mod total;
---
+++ src/cmp/total.rs
@@ -0,0 +1,25 @@
+use std::ops::{Deref, DerefMut};
+
+#[derive(PartialEq, PartialOrd)]
+pub struct Total<T>(pub T);
+
+impl<T: PartialEq> Eq for Total<T> {}
+
+impl<T: PartialOrd> Ord for Total<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.partial_cmp(&other.0).unwrap()
+    }
+}
+
+impl<T> Deref for Total<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Total<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
