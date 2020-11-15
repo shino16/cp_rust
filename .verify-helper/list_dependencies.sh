ef196ed0c5bc51a5b0d3e33e7a6b034266887bfd
shino16 <masatosinokawa@gmail.com>
2020-11-16 02:34:02 +0900

    .

--- .verify-helper/list_dependencies.sh
+++ .verify-helper/list_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cargo install --git https://github.com/shino16/cargo-mod-deps.git --branch main
+cargo install -q --git https://github.com/shino16/cargo-mod-deps.git --branch main
 cargo-mod-deps --extract lib --target "$(basename "$1" | cut -f 1 -d '.')"
