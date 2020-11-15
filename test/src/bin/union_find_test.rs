f3ed1fa57e983ed1455dd51802bb5c188b126e42
shino16 <masatosinokawa@gmail.com>
2020-11-16 01:31:27 +0900

    add tests

---
+++ test/src/bin/union_find_test.rs
@@ -0,0 +1,18 @@
+// verify-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A
+
+use lib::ds::uf::*;
+use lib::io::*;
+
+fn main() {
+    let mut io = IO::new();
+    let (n, q) = io.scan();
+    let mut uf = UnionFind::new(n);
+    for _ in 0_usize..q {
+        let (com, x, y): (u8, _, _) = io.scan();
+        if com == b'0' {
+            uf.unite(x, y);
+        } else {
+            io.println(uf.is_same(x, y) as u32);
+        }
+    }
+}
