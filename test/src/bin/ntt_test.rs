597a2f1c6ec74eb7fcc8d0ea0711148ea760c625
shino16 <masatosinokawa@gmail.com>
2020-11-16 02:22:42 +0900

    add test

---
+++ test/src/bin/ntt_test.rs
@@ -0,0 +1,15 @@
+use lib::io::*;
+use lib::fp::*;
+use lib::math::ntt::*;
+
+fn main() {
+    let mut io = IO::new();
+    let (n, m) = io.scan();
+    let a = io.scan_vec::<Fp99>(n);
+    let b = io.scan_vec::<Fp99>(m);
+    if (n, m) == (1, 1) {
+        io.println(a[0] * b[0]);
+    } else {
+        io.iterln(Conv::conv(a, b).into_iter(), " ");
+    }
+}
